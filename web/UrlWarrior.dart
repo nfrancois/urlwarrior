3f49729ca528593c177aefcaf8d2b3ec41715607
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-04-01 23:30:03 +0200

    Introduce game manager

--- web/UrlWarrior.dart
+++ web/UrlWarrior.dart
@@ -2,13 +2,70 @@ import "dart:html";
 import "dart:math";
 import "dart:async";
 
-class UrlWarrior {
+
+main() {
+  var gameManager = new UrlWarriorGameManager(window.location);
+  window.onKeyUp.listen(gameManager._onKeyUp);
+}
+
+class UrlWarriorGameManager {
+  
+  const String _WELCOME_MSG = "The Game is in the url. Press space to start !";
+  final UrlWarriorGame _game;
+  Timer _gameTimer;
+  final Location location;
+  //int _speed;
+  
+  UrlWarriorGameManager(this.location) : _game = new UrlWarriorGame() {
+    location.hash = _WELCOME_MSG;
+  }
+  
+  _onKeyUp(KeyboardEvent keyboardEvent){
+    int keyCode = keyboardEvent.keyCode;
+    if(KeyCode.A<=keyCode && keyCode<=KeyCode.Z){
+      _game.reduce(keyCode);
+      _refreshDisplay();
+    } else if(!_game.running && keyCode == KeyCode.SPACE){
+      _start();
+    }
+  }
+  
+  _start(){
+    _game.start();
+    if(_gameTimer != null){
+      _gameTimer.cancel();
+    }
+    _gameTimer = new Timer.periodic(new Duration(seconds: 1), (_) => _gameLoop());
+  }
+  
+  _gameLoop(){
+    if(_game.running){
+      _game.generate();
+      _refreshDisplay();
+    } else {
+      _displayEndScore();
+    }
+  }  
+  
+  _refreshDisplay(){
+    location.hash = "Score=${_game.score}#${_game.sequence}";  
+  }
+
+  _displayEndScore(){
+    location.hash = "Score=${_game.score}";  
+  }  
+  
+}
+
+class UrlWarriorGame {
 
   const int _MAX_SIZE = 50;
   List<int> _sequence = [];
   int _score = 0;
   Random _random = new Random();
   bool _running = false;
+  
+  UrlWarriorGame();
   
   start(){
     _sequence = [];  
@@ -42,47 +99,4 @@ class UrlWarrior {
   
   bool get running => _running;
   
-}
-
-UrlWarrior _game = new UrlWarrior();
-Timer _gameTimer;
-
-main() {
-  window.location.hash = "The Game is in the url. Press space to start !";
-  window.onKeyDown.listen(_onKeyDown);
-}
-
-_start(){
-  _game.start();
-  if(_gameTimer != null){
-    _gameTimer.cancel();
-  }
-  _gameTimer = new Timer.periodic(new Duration(seconds: 1), (_) => _gameLoop());
-}
-
-_gameLoop(){
-  if(_game.running){
-    _game.generate();
-    _refreshDisplay();
-  } else {
-    _displayEndScore();
-  }
-}
-
-_refreshDisplay(){
-  window.location.hash = "Score=${_game.score}#${_game.sequence}";  
-}
-
-_displayEndScore(){
-  window.location.hash = "Score=${_game.score}";  
-}
-
-_onKeyDown(KeyboardEvent keyboardEvent){
-  int keyCode = keyboardEvent.keyCode;
-  if(KeyCode.A<=keyCode && keyCode<=KeyCode.Z){
-    _game.reduce(keyCode);
-    _refreshDisplay();
-  } else if(!_game.running && keyCode == KeyCode.SPACE){
-    _start();
-  }
 }
